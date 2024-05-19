2f5c27bc6fc7e8e0c371f1ba9176555c3bd46512
Azizjon <aliqulovazizjon68@gmail.com>
2024-05-19 22:36:52 +0500

    Refactor package.json and src/main.ts for better code organization and functionality

--- src/scenes/checkUser.ts
+++ src/scenes/checkUser.ts
@@ -1,3 +1,4 @@
+import { create } from "domain";
 import { Scenes } from "telegraf";
 import enabled from "../utils/enabled";
 import prisma from "../../prisma/prisma";
@@ -47,5 +48,32 @@ scene.hears(/^[0-9]{1,10}$/, async (ctx: any) => {
   };
 
   return ctx.scene.enter("addBalance");
+});
+
+scene.hears("Bugungi tushunmi ko'rish", async (ctx) => {
+  const userId = String(ctx.from.id);
+
+  if (userId !== externalId) {
+    return;
+  }
+
+  const walletBalance = await prisma.walletRequest.findMany({
+    where: {
+      status: "APPROVED",
+      created_at: {
+        gte: new Date(new Date().setHours(0, 0, 0, 0)),
+        lt: new Date(new Date().setHours(23, 59, 59, 999)),
+      },
+    },
+  });
+
+  let balance = 0;
+
+  walletBalance.forEach((item) => {
+    balance += item.amount;
+  });
+
+  const text = `Bugungi tushum: ${balance}`;
+  await ctx.telegram.sendMessage(userId, text);
 });
 export default scene;
